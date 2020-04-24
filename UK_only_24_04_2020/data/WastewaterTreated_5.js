4ed8312eed2f0c8ed8b9290bdbe34217c1315c5a
NCLwaterqualitymaps <63046429+NCLwaterqualitymaps@users.noreply.github.com>
2020-04-24 13:53:54 +0100

    Delete WastewaterTreated_5.js

--- UK_only_24_04_2020/data/WastewaterTreated_5.js
+++
@@ -1,9 +0,0 @@
-var json_WastewaterTreated_5 = {
-"type": "FeatureCollection",
-"name": "WastewaterTreated_5",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Sample_ID": "S1_2019", "Sampling date": "14.03.2019", "Location ID": "Birtley WWTP", "Sample Source": "Wastewater (treated)", "Contaminants": "High faecal coliform count; High nitrate level; ", "Health Effects": "Faecal contamination can lead to vomiting and diarrhoea. Pathogens such as E coli, hepatitis and salmonella can cause severe health effects ; High Nitrate levels can lead to cyanosis, asphyxia and blue-baby syndrome in infants; ", "Treatments": "Faecal treatment - Heat water to a rolling boil and then allow to cool naturally, chemical disinfection, coagulation, distillation, reverse osmosis, slow sand filtration or solar disinfection; Nitrate can be reduced through ion exchange; " }, "geometry": { "type": "Point", "coordinates": [ -1.6021, 54.90472 ] } },
-{ "type": "Feature", "properties": { "Sample_ID": "S7", "Sampling date": "19.06.2019", "Location ID": "Birtley WWTP", "Sample Source": "Wastewater (treated)", "Contaminants": "High faecal coliform count; ", "Health Effects": "Faecal contamination can lead to vomiting and diarrhoea. Pathogens such as E coli, hepatitis and salmonella can cause severe health effects ; ", "Treatments": "Faecal treatment - Heat water to a rolling boil and then allow to cool naturally, chemical disinfection, coagulation, distillation, reverse osmosis, slow sand filtration or solar disinfection; " }, "geometry": { "type": "Point", "coordinates": [ -1.6021, 54.90472 ] } }
-]
-}
